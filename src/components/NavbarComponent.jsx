5946c507928d8a98786cb1151682385d95f0e2da
JovanMwesigwa <jovanmwesigwa79@gmail.com>
2023-02-22 16:31:34 +0300

    Added webuikit connect button and morales-v1

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,10 +1,12 @@
+import { Avatar, ConnectButton } from 'web3uikit'
+
 const NavbarComponent = () => {
   return (
     <nav className="w-full h-16 items-center justify-between px-8 flex flex-row p-2">
       <div className="flex flex-1">
         <h2 className="font-medium text-lg">CELO-SWAP</h2>
       </div>
-      <button>Connect Wallet</button>
+      <ConnectButton moralisAuth={false} />
     </nav>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { MoralisProvider } from 'react-moralis'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MoralisProvider initializeOnMount={false}>
+      <App />
+    </MoralisProvider>
+  </React.StrictMode>
 )
